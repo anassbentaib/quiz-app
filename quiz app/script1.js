5aaab767774ac1449bc6321f795738ed0c20a472
Ben_Taib_Anass <89553561+anassbentaib@users.noreply.github.com>
2022-07-18 23:43:51 +0200

    Add files via upload

---
+++ quiz app/script1.js
@@ -0,0 +1,40 @@
+const quizData =[
+    {
+        question:'how old is florin',
+        a:'10',
+        b:'20',
+        c:'31',
+        d:'665',
+        correct : 'c',
+         
+    }, {
+        question:'what is the must used programming language in 2019?',
+
+        a: 'java',
+        b:'C',
+        b:'python',
+        d:'javascript',
+    },{
+    question:'who is the president of US?',
+    a:'florin Pop',
+    b:'Donald Trump',
+    c:'Ivan saldano',
+    d:'mihai andrie',
+    },{
+        question:'what does HTML stand for',
+        a:'hyperText marckup language',
+        b:'cascading style sheet',
+        c:'jason object notation', 
+        d:'helicopter terminals motor boats lamborgints',
+        correct :'a'
+    },{
+        question:'what year was javascript launched',
+        a:'2019',
+        b:'2020',
+        c:'2018',
+        d:'none about the above',
+        correct :'d'
+
+    }
+
+]
